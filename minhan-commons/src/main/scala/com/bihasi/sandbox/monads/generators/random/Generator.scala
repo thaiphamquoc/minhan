1207af5a122649faab21c61bcce5ee89cf1e1539
Thai Pham <thapham@adobe.com>
2019-11-12 01:02:27 -0800

    update pom

--- minhan-commons/src/main/scala/com/bihasi/sandbox/monads/generators/random/Generator.scala
+++ minhan-commons/src/main/scala/com/bihasi/sandbox/monads/generators/random/Generator.scala
@@ -1,5 +1,14 @@
 package com.bihasi.sandbox.monads.generators.random
 
 trait Generator[+T] {
+  self =>
   def generate: T
+
+  def map[S](f: T => S): Generator[S] = new Generator[S] {
+    override def generate: S = f(self.generate)
+  }
+
+  def flatMap[S](f: T => Generator[S]) = new Generator[S] {
+    override def generate: S = f(self.generate).generate
+  }
 }
--- minhan-commons/src/test/scala/com/bihasi/sandbox/monads/generators/random/GeneratorSpec.scala
+++ minhan-commons/src/test/scala/com/bihasi/sandbox/monads/generators/random/GeneratorSpec.scala
@@ -5,16 +5,28 @@ import org.scalatest.{Matchers, WordSpecLike}
 
 class GeneratorSpec extends MockFactory with WordSpecLike with Matchers {
 
-  val integers = new Generator[Int] {
-    val rand = new java.util.Random
-    override def generate: Int = rand.nextInt()
+  private trait BaseContext {
+    val integers = new Generator[Int] {
+      val rand = new java.util.Random
+      override def generate: Int = rand.nextInt()
+    }
+  }
+
+  private trait BooleanGeneratorContext extends BaseContext {
+    val booleans = for (x <- integers) yield x > 0
   }
 
   "integers" should {
-    "generate a random integer" in {
+    "generate a random integer" in new BaseContext {
       val randomInt = integers.generate
       println(s"Random integer: $randomInt")
       assert(randomInt * 1 == randomInt)
+    }
+  }
+
+  "booleans" should {
+    "generate a randon boolean value" in new BooleanGeneratorContext {
+      println(booleans.generate)
     }
   }
 }
